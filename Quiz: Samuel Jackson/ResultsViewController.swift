2c65cc0907816c3b0f01459d5e13e3a01e9e437c
Enzo Maruffa <enzomm1999@gmail.com>
2019-05-12 14:16:09 -0300

    results now working

--- Quiz: Samuel Jackson/ResultsViewController.swift
+++ Quiz: Samuel Jackson/ResultsViewController.swift
@@ -11,11 +11,33 @@ import UIKit
 class ResultsViewController: UIViewController {
 
     @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var player1name: UILabel!
+    @IBOutlet weak var player1points: UILabel!
+    @IBOutlet weak var player2name: UILabel!
+    @IBOutlet weak var player2points: UILabel!
+    @IBOutlet weak var player3name: UILabel!
+    @IBOutlet weak var player3points: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let resultsShown = AppData.sharedInstance.results.prefix(3)
+        
+        if resultsShown.count > 0  {
+            player1name.text = resultsShown[0].name
+            player1points.text = resultsShown[0].points.description
+        }
+        
+        if resultsShown.count > 1  {
+            player2name.text = resultsShown[1].name
+            player2points.text = resultsShown[1].points.description
+        }
+        
+        if resultsShown.count > 2  {
+            player3name.text = resultsShown[2].name
+            player3points.text = resultsShown[2].points.description
+        }
     }
     
 
